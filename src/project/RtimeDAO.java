cb109a7f6711c85f3ca9560adf617aeeb822d235
KimGyeongHa <84758994+KimGyeongHa@users.noreply.github.com>
2021-08-09 01:33:30 +0900

    Update RtimeDAO.java

--- src/project/RtimeDAO.java
+++ src/project/RtimeDAO.java
@@ -8,10 +8,10 @@ import java.sql.SQLException;
 
 public class RtimeDAO {
 		String driver = "oracle.jdbc.driver.OracleDriver";
-		String url = "jdbc:oracle:thin:@db1.cwofzefzc3ci.ap-northeast-2.rds.amazonaws.com:1521:db1";
-		String user = "jadmin";
-		String password = "tiger999";
-		Connection conn = null; //java.sql�뿉 �엳�뒗寃껋쑝濡� import
+		String url = "";
+		String user = "";
+		String password = "";
+		Connection conn = null; 
 		PreparedStatement pstmt = null;
 		StringBuffer sb = new StringBuffer();
 		ResultSet rs = null;
@@ -34,7 +34,7 @@ public class RtimeDAO {
 			sb.setLength(0);
 			sb.append("SELECT RNO, TIME, BKTIME ");
 			sb.append(" FROM RTIME WHERE RNO = ? ");
-			 // 議곌굔�뿉 留뚯”�븯�뒗 �뻾 �븳媛쒕쭔 異쒕젰
+
 			RtimeVO vo = null;
 			try {
 				pstmt = conn.prepareStatement(sb.toString());
@@ -48,7 +48,6 @@ public class RtimeDAO {
 				String bktime = rs.getString("BKTIME");
 				
 		
-				// no�뒗 寃뚯떆臾� 踰덊샇
 				vo = new RtimeVO(no, time, bktime);
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
